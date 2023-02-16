d96db9b09257e2420fa24a41cf3736849e69dedf
muratcan-yuksel <muratcanyukselpro@gmail.com>
2023-02-17 02:51:21 +0300

    style items

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useQuery } from "react-query";
 //components
+import Item from "./Item/Item";
 import Drawer from "@mui/material/Drawer";
 import LinearProgress from "@mui/material/LinearProgress";
 import Grid from "@mui/material/Grid";
@@ -30,7 +31,29 @@ const App = () => {
     getProducts
   );
   console.log(data);
-  return <div>App</div>;
+
+  const getTotalItems = () => null;
+
+  const handleAddToCart = (clickedItem: CartItemType) => null;
+
+  const handleRemoveFromCart = () => null;
+
+  if (isLoading) return <LinearProgress />;
+  if (error) return <div>Something went wrong...</div>;
+
+  return (
+    <div>
+      <Wrapper>
+        <Grid container spacing={3}>
+          {data?.map((item) => (
+            <Grid item key={item.id} xs={12} sm={4}>
+              <Item item={item} handleAddToCart={handleAddToCart} />
+            </Grid>
+          ))}
+        </Grid>
+      </Wrapper>
+    </div>
+  );
 };
 
 export default App;
